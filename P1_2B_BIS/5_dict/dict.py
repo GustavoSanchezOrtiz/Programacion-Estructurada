079c48f58a8167c2b7a1d738f46bc238605e47ab
GustavoSanchezOrtiz <gustavo_3141240147@utd.edu.mx>
2025-07-23 08:36:56 -0600

    Delete P1_2B_BIS directory

--- P1_2B_BIS/5_dict/dict.py
+++
@@ -1,4 +0,0 @@
-'''dict.- es un tipo de datos que se utiliza par almacenar datos de diferente tipo de datos pero en ligar de tener
-como las listas indices numericos tiene alfanumericos. Es decir es algo parecido como los objetos
-tambien se conoce como un arreglo asosiativo u objeto JSON
-el diccionario es una coleccion ordenada y modificables'''
--- P1_2B_BIS/6_tuple/tuple.py
+++
@@ -1,3 +0,0 @@
-''' las tuplas se utilizzan para lamacenar varios elementos en una sola variable.
-una tupla es una coleccion ordenada e i9nmutable.
-las tuplas se escriben entre parentesis'''
